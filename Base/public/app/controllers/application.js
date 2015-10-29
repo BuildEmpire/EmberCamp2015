9649b4bb4569517d0c22d785a2cb5c601062cd95
Fred Thompson <fred.thompson@buildempire.co.uk>
2015-10-29 11:46:50 +0000

    Ok, so this example works. Isnt mad exciting though.

---
+++ Base/public/app/controllers/application.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  groupedNumbers: [
+    { groupName: "Smalls", options: ["one", "two", "three"] },
+    { groupName: "Mediums", options: ["four", "five", "six"] },
+    { groupName: "Bigs", options: [
+      { groupName: "Fairly big", options: ["seven", "eight", "nine"] },
+      { groupName: "Really big", options: [ "ten", "eleven", "twelve" ] },
+      "thirteen"
+    ]
+    },
+    "one hundred",
+    "one thousand"
+  ],
+  actions: {
+    foo() {
+      console.log("in foo");
+
+    }
+  }
+});
